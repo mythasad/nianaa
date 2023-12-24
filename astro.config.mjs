753016283c8be4cc189282cc95b5ad3eeae950c3
mythasad <mythasad@gmail.com>
2023-12-25 00:44:14 +0600

    configuration update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
 
-export default defineConfig({});
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx(), sitemap()]
+});
